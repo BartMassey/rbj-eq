c61cbb80ee6c92d9bdf2735544d88e5baf3557f8
Bart Massey <bart.massey@gmail.com>
2022-01-16 20:22:17 -0800

    finished all filter types, cleaned up example

--- examples/sweep.rs
+++ examples/sweep.rs
@@ -6,15 +6,15 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let f0: f64 = args[1].parse().unwrap();
     let fs = 48_000;
-    let cs = basic_filter_coeffs(
-        BasicFilterType::BandPassQ,
+    let cs = filter_coeffs(
+        Filter::Basic(BasicFilter::BandPassQ),
         fs as f64,
         f0,
-        BasicFilterWidth::Q(1.414),
+        FilterWidth::Q(1.414),
     );
-    let mut filter = make_filter(cs);
+    let filter = make_filter(cs);
 
-    let x: Vec<f64> = (0..fs)
+    let x = (0..fs)
         .map(|i| {
             // https://en.wikipedia.org/wiki/Chirp#Linear
             let f0 = 100.0_f64;
@@ -22,10 +22,9 @@ fn main() {
             let c = 0.5 * (f1 - f0);
             let t = i as f64 / fs as f64;
             f64::sin(2.0 * PI * (c * t * t + f0 * t))
-        })
-        .collect();
+        });
 
-    let y: Vec<f64> = x.into_iter().map(|x| filter(x)).collect();
-
-    y.into_iter().for_each(|y| println!("{}", y));
+    for y in x.into_iter().map(filter) {
+        println!("{}", y);
+    }
 }
